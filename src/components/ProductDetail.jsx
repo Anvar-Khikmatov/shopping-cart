81cc35712eda381929ab93708b8611bcca53f941
Anvar Khikmatov <Anvar-Khikmatov@users.noreply.github.com>
2026-09-13 21:44:48 +0500

    implemented img thumbnail for different screen sizes

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router'
+import { data, useParams } from 'react-router'
 
 
 function ProductDetail() {
 
   const [product, setProduct] = useState(null)
+  const [currentImg, setCurrentImg] = useState(null)
   const { id } = useParams()
+  const imgCarouselClass = " h-27 w-27 cursor-pointer bg-black/8 rounded transition-all duration-300  border hover:border-amber-500"
 
   useEffect(() => {
     async function fetchProduct() {
@@ -16,6 +18,7 @@ function ProductDetail() {
         }
         const data = await response.json()
         setProduct(data)
+        setCurrentImg(data.images[0])
 
       } catch(error) {
         console.error("Fetch process: ", error.message)
@@ -23,32 +26,48 @@ function ProductDetail() {
     }
     fetchProduct()
   }, [])
+
+  
+  const handleImgClick = (i) => {
+    setCurrentImg(product.images[i])
+  }
   
   
   return (
     <section className='flex justify-center items-center py-12 xl:py-16 px-(--section-mobile-px) xl:px-(--section-content-px)'>
       {!product ? null : 
-        <div className='container max-w-400 grid grid-cols md:grid-cols-2 justify-center items-center  gap-6'>
-          <div className='relative bg-black/5 w-full h-70 lg:h-110 xl:h-150 overflow-hidden'>
-            <img 
-              src={product.images[0]} 
-              alt={product.title} 
-              className='w-full h-full object-cover xl:object-[center_180%] scale-90' />
-            <div className='absolute bottom-6 left-1/2 -translate-x-1/2 bg-amber-400 w-fit flex gap-4'>
-              {product.images.map((item, i) => (
-                <button 
-                  key={`img-indicator-${i}`}
-                  className='border h-27 w-27 cursor-pointer z-10'    
-                >
-                  <img src={item} />
-                </button>
-              ))}
+        <div className='container max-w-400 grid grid-cols-1 md:grid-cols-2 justify-center items-center  gap-6'>
 
-              {/* <div className='border h-27 w-27'></div>
-              <div className='border h-27 w-27'></div>
-              <div className='border h-27 w-27'></div> */}
+          <div className='flex flex-col items-center gap-4'>
+            <div className='relative bg-black/5 w-full h-70 lg:h-110 xl:h-150 rounded xl:rounded-2xl overflow-hidden'>
+              <img 
+                src={currentImg} 
+                alt={product.title} 
+                className='w-full h-full object-cover xl:object-[center_180%] scale-80' />
+
+              <div className='hidden md:flex absolute md:bottom-6 md:left-1/2 -translate-x-1/2  w-fit gap-4'>
+                {product.images.map((item, i) => (
+                  <button 
+                    key={`img-indicator-${i}`}
+                    className={`${imgCarouselClass} ${product.images[i] === currentImg ? 'border'  : 'border-transparent' }`}
+                    onClick={() => handleImgClick(i)}>
+                    <img src={item} />
+                  </button>
+                ))}
+              </div>
             </div>
-          </div>
+
+            <div className=' flex justify-center items-center w-fit gap-4 md:hidden'>
+                {product.images.map((item, i) => (
+                  <button 
+                    key={`img-indicator-${i}`}
+                    className={`${imgCarouselClass} ${product.images[i] === currentImg ? 'border'  : 'border-transparent' }`}
+                    onClick={() => handleImgClick(i)}>
+                    <img src={item} />
+                  </button>
+                ))}
+              </div>
+            </div>  
 
           <div className='flex flex-col justify-center'>
             <div>subheading</div>
